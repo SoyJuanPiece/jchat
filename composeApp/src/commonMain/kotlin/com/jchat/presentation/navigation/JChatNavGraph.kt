393b832a747aeab888fdf74c40bc2371bd9e7055
vivenesjuan332 <vivenesjuan332@gmail.com>
2026-03-13 05:08:26 +0000

    fix: add missing launch import in JChatNavGraph

--- composeApp/src/commonMain/kotlin/com/jchat/presentation/navigation/JChatNavGraph.kt
+++ composeApp/src/commonMain/kotlin/com/jchat/presentation/navigation/JChatNavGraph.kt
@@ -6,6 +6,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import kotlinx.coroutines.launch
 import com.jchat.presentation.AuthScreen
 import com.jchat.presentation.conversation.ConversationScreen
 import com.jchat.presentation.profile.ProfileScreen
